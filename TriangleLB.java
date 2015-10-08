6f1d3e542e925d076950c1da90c8c22022bf4177
HorieYuhei <yuhei.aiki@gmail.com>
2015-10-08 19:00:56 +0900

    Update TriangleLB.java

--- TriangleLB.java
+++ TriangleLB.java
@@ -1,22 +1,23 @@
-import java.util.Scanner;
+import java.util.Scanner; //スキャナクラスのインポート宣言
 
-public class TriangleLB {
-	public static void main(String[] args) {
-		Scanner stdIn = new Scanner(System.in);
-		int n;
+public class TriangleLB {//三角形クラス
+	public static void main(String[] args) {//メイン関数
+		Scanner stdIn = new Scanner(System.in); //スキャナクラスのインスタンス生成
+		int n; //段数入力のための変数n
 
-		System.out.println("左下直角の三角形を表示します。");
+		System.out.println("左下直角の三角形を表示します。");//見出し
 
-		do {
+		do {//段数が正になるまで入力を催促
 			System.out.print("段数は :");
-			n = stdIn.nextInt();
+			n = stdIn.nextInt();//整数値の読み込み
 		} while (n <= 0);
 
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {//二重ループ
 			for (int j = 1; j <= i; j++) {
+			//iの値がふえるごとに横方向の繰り返しも増える
 					System.out.print('*');
 			}
-				System.out.println();
+				System.out.println();//改行
 			
 		}
 	}
