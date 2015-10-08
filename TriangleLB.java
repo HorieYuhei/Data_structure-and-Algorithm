e99b63ab8b6f663efdc3880ba8261bc94a7d125b
HorieYuhei <yuhei.aiki@gmail.com>
2015-10-08 18:35:55 +0900

    Update TriangleLB.java

--- TriangleLB.java
+++ TriangleLB.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
+
+public class TriangleLB {
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int n;
+
+		System.out.println("左下直角の三角形を表示します。");
+
+		do {
+			System.out.print("段数は :");
+			n = stdIn.nextInt();
+		} while (n <= 0);
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+					System.out.print('*');
+			}
+				System.out.println();
+			
+		}
+	}
+}
 
